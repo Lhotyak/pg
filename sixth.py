c5a2a4dcaad74593e502962a6c7a014a9966f6d6
Lhotka <adam.lhotka@gmail.com>
2025-11-18 21:41:57 +0000

    sixth

---
+++ sixth.py
@@ -0,0 +1,41 @@
+
+import sys
+import requests
+import re
+
+
+def download_url_and_get_all_hrefs(url):
+    """
+    Funkce stahne url predanou v parametru url pomoci volani response = requests.get(),
+    zkontroluje navratovy kod response.status_code, ktery musi byt 200,
+    pokud ano, najdete ve stazenem obsahu stranky response.content vsechny vyskyty
+    <a href="url">odkaz</a> a z nich nactete url, ktere vratite jako seznam pomoci return
+    """
+    hrefs = []
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise Exception("Chyba při stahování stránky")
+
+    html = response.text
+
+    found_links = re.findall(r'href="([^"]*)"', html)
+
+    for link in found_links:
+        hrefs.append(link)
+
+    return hrefs
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        hrefs = download_url_and_get_all_hrefs(url)
+
+        for h in hrefs:
+            print(h)
+
+    except Exception as e:
+        print("Program skončil chybou:", e)
+
